0bbca4ca877a9c722cf6249e3709b8a919290034
Yuri Prawucki <yuriprawucki1@gmail.com>
2022-04-29 20:30:30 -0300

    Update 4-Verificar_WSL.ps1

--- 4-Verificar_WSL.ps1
+++ 4-Verificar_WSL.ps1
@@ -1,31 +1,32 @@
-# Verifica instalação do WSL
+# Verifica instalaÃ§Ã£o do WSL
 if ((Get-WindowsOptionalFeature -FeatureName "Microsoft-Windows-Subsystem-Linux" -Online).State -eq "Enabled"){
     echo "============================================="
     echo "WSL Instalado"
     }
     else{
     echo "============================================="
-    echo "WSL Não Instalado"
+    echo "WSL NÃ£o Instalado"
 }
 
-# Verifica instalação da Plataforma de Virtualização
+# Verifica instalaÃ§Ã£o da Plataforma de VirtualizaÃ§Ã£o
 if ((Get-WindowsOptionalFeature -FeatureName "VirtualMachinePlatform" -Online).State -eq "Enabled"){
     echo "============================================="
-    echo "Plataforma de Virtualização Instalada"
+    echo "Plataforma de VirtualizaÃ§Ã£o Instalada"
     }
     else{
     echo "============================================="
-    echo "Plataforma de Virtualização Não Instalada"
+    echo "Plataforma de VirtualizaÃ§Ã£o NÃ£o Instalada"
 }
 
-# Verifica instalação do Ubuntu
+# Verifica instalaÃ§Ã£o do Ubuntu
 echo "============================================="
 if ((ls 'C:\Program Files\WindowsApps\' | where Name -Like CanonicalGroupLimited.Ubuntu* | where Name -Like *_x64__* | Format-List Name) -ne ""){
     echo "Ubuntu Instalado"
-    echo "--- Agora abra o Ubuntu para inicializar"
+    echo "--- Inicializando o Ubuntu"
+    start ubuntu
     }
     else{
-    echo "Ubuntu Não Instalado"
+    echo "Ubuntu NÃ£o Instalado"
 }
 echo "============================================="
 
